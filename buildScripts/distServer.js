e373458edff8b05f515b9dee24864507a88a5385
Kevin Garay <kevin.garay@tegraglobal.com>
2018-05-31 09:56:43 -0600

    Deploy ready

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -8,19 +8,11 @@ import compression from 'compression';
 const port = 3000;
 const app = express();
 
-app.unsubscribe(compression());
+app.use(compression());
 app.use(express.static('dist'));
 
 app.get('/', function(req,res){
   res.sendFile(path.join(__dirname,'../dist/index.html'));
-})
-
-app.get('/users', (req,res) => {
-  res.json([
-    {"id":1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id":2,"firstName":"Bob2","lastName":"Smith2","email":"bob2@gmail.com"},
-    {"id":3,"firstName":"Bob3","lastName":"Smith3","email":"bob3@gmail.com"}
-  ]);
 });
 
 app.listen(port, function(err){
